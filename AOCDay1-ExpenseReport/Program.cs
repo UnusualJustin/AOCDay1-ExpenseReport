47d1abd741be6735a1db0fabb0192846b5bb7b8b
Justin Cook <justin.w.cook.civ@mail.mil>
2020-12-03 08:27:40 -0600

    Added abort logic to prevent erroneous searching and speed things up a bit.

--- AOCDay1-ExpenseReport/Program.cs
+++ AOCDay1-ExpenseReport/Program.cs
@@ -14,21 +14,38 @@ namespace AOCDay2_ExpenseReport
                                 .OrderBy(v => v)
                                 .ToArray();
 
-            List<int> addends = FindAddends(2020, values, 2);
-            Console.WriteLine("{0} = 2020; {1} = {2}", 
+            FindAddendsAndMultiply(values, 2020, 2);
+            FindAddendsAndMultiply(values, 2020, 3);
+            FindAddendsAndMultiply(values, 3852, 4);
+            FindAddendsAndMultiply(values, 5145, 5);
+            FindAddendsAndMultiply(values, 6517, 6);
+            FindAddendsAndMultiply(values, 7400, 7);
+            FindAddendsAndMultiply(values, 10000, 8);
+        }
+
+        static void FindAddendsAndMultiply(int[] values, int sum, int numberOfAddends)
+        {
+            List<int> addends = FindAddends(sum, values, numberOfAddends);
+            if (addends != null)
+            {
+                Console.WriteLine("{0} = 2020; {1} = {2}",
                               string.Join(" + ", addends.Select(a => a.ToString())),
                               string.Join(" * ", addends.Select(a => a.ToString())),
                               CalculateProduct(addends));
-
-            List<int> addends2 = FindAddends(2020, values, 3);
-            Console.WriteLine("{0} = 2020; {1} = {2}", 
-                              string.Join(" + ", addends2.Select(a => a.ToString())),
-                              string.Join(" * ", addends2.Select(a => a.ToString())),
-                              CalculateProduct(addends2));
+            }
+            else
+            {
+                Console.Error.WriteLine($"No combination of {numberOfAddends} numbers found that equals {sum}!");
+            }
         }
 
         static List<int> FindAddends(int sum, int[] values, int numberOfAddends)
         {
+            if (values.Length == 0 || values[0] * numberOfAddends > sum)
+            {
+                return null;
+            }
+
             if (numberOfAddends == 2)
             {
                 for (int i = 0; i < values.Length; i++)
@@ -46,7 +63,7 @@ namespace AOCDay2_ExpenseReport
             {
                 for (int i = 0; i < values.Length; i++)
                 {
-                    List<int> appends = FindAddends(sum - values[i], values.Skip(i).ToArray(), numberOfAddends - 1);
+                    List<int> appends = FindAddends(sum - values[i], values.Skip(i + 1).ToArray(), numberOfAddends - 1);
                     if (appends != null)
                     {
                         appends.Insert(0, values[i]);
@@ -58,9 +75,15 @@ namespace AOCDay2_ExpenseReport
             return null;
         }
 
-        static int CalculateProduct(List<int> factors)
-        { 
-            return factors.Aggregate(1, (x, y) => x * y);
+        static long CalculateProduct(List<int> factors)
+        {
+            long product = 1;
+            foreach (int factor in factors)
+            {
+                product *= factor;
+            }
+
+            return product;
         }
     }
 }
