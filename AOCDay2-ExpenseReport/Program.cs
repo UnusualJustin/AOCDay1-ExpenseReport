3c9f2db8e7eac2723e444e27b6ac093ff2ae0722
Justin Cook <justin.w.cook.civ@mail.mil>
2020-12-02 20:38:21 -0600

    Add project files.

---
+++ AOCDay2-ExpenseReport/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AOCDay2_ExpenseReport
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = File.ReadAllLines("input.txt");
+            int[] values = lines.Select(l => int.Parse(l))
+                                .OrderBy(v => v)
+                                .ToArray();
+
+            List<int> addends = FindAddends(2020, values, 2);
+            Console.WriteLine("{0} = 2020; {1} = {2}", 
+                              string.Join(" + ", addends.Select(a => a.ToString())),
+                              string.Join(" * ", addends.Select(a => a.ToString())),
+                              CalculateProduct(addends));
+
+            List<int> addends2 = FindAddends(2020, values, 3);
+            Console.WriteLine("{0} = 2020; {1} = {2}", 
+                              string.Join(" + ", addends2.Select(a => a.ToString())),
+                              string.Join(" * ", addends2.Select(a => a.ToString())),
+                              CalculateProduct(addends2));
+        }
+
+        static List<int> FindAddends(int sum, int[] values, int numberOfAddends)
+        {
+            if (numberOfAddends == 2)
+            {
+                for (int i = 0; i < values.Length; i++)
+                {
+                    for (int j = values.Length - 1; j > i; j--)
+                    {
+                        if (values[i] + values[j] == sum)
+                        {
+                            return new List<int> { values[i], values[j] };
+                        }
+                    }
+                }
+            }
+            else
+            {
+                foreach (int value in values)
+                {
+                    List<int> appends = FindAddends(sum - value, values, numberOfAddends - 1);
+                    if (appends != null)
+                    {
+                        appends.Insert(0, value);
+                        return appends;
+                    }
+                }
+            }
+
+            return null;
+        }
+
+        static int CalculateProduct(List<int> factors)
+        { 
+            return factors.Aggregate(1, (x, y) => x * y);
+        }
+    }
+}
